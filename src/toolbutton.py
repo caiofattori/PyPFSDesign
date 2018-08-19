90a99661a8683d6137b89bc191c9565c94b514aa
Caio Fattori <caio.fattori@gmail.com>
2018-08-19 11:32:24 -0300

    issue3: all element buttons created and add qtabwidget to central widget of window

--- src/toolbutton.py
+++ src/toolbutton.py
@@ -11,3 +11,25 @@ class PFSActivityButton(QToolButton):
 		p.setPen(Qt.black)
 		p.setFont(QFont("Helvetica", 12))
 		p.drawText(self.rect(), Qt.AlignCenter, "[Ac]")
+		
+class PFSDistributorButton(QToolButton):
+	def __init__(self):
+		super(QToolButton, self).__init__()
+	
+	def paintEvent(self, ev):
+		p = QPainter(self)
+		p.setPen(Qt.black)
+		r = self.rect()
+		p.drawEllipse(r.left()+5, r.top()+5, r.width()-10, r.height()-10)
+		
+class PFSRelationButton(QToolButton):
+	def __init__(self):
+		super(QToolButton, self).__init__()
+	
+	def paintEvent(self, ev):
+		p = QPainter(self)
+		p.setPen(Qt.black)
+		r = self.rect()
+		p.drawLine(r.left() + 5, r.bottom() - 5, r.right() - 5, r.top() + 5)
+		p.drawLine(r.right() - 15, r.top() + 10, r.right() - 5, r.top() + 5)
+		p.drawLine(r.right() - 10, r.top() + 15, r.right() - 5, r.top() + 5)
--- src/window.py
+++ src/window.py
@@ -1,21 +1,27 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QToolBar, QWidgetAction, QMainWindow
-from toolbutton import PFSToolButton
+from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QToolBar, QMainWindow, QTabWidget
+from toolbutton import PFSActivityButton, PFSDistributorButton, PFSRelationButton
 
 class PFSWindow(QWidget):
 	def __init__(self):
 		super(QWidget, self).__init__()
-		mainLayout = QVBoxLayout()
-		
+		mainLayout = QHBoxLayout()
 		self.setLayout(mainLayout)
+		self._tab = QTabWidget()
+		w = QWidget()
+		self._tab.addTab(w, "Teste")
+		mainLayout.addWidget(self._tab)
 		
 class PFSMain(QMainWindow):
 	def __init__(self):
 		super(QMainWindow, self).__init__()
 		toolBar = QToolBar()
-		
 		toolBar = self.addToolBar("teste")
 		ac = toolBar.addWidget(PFSActivityButton())
 		ac.setVisible(True)
+		ac = toolBar.addWidget(PFSDistributorButton())
+		ac.setVisible(True)
+		ac = toolBar.addWidget(PFSRelationButton())
+		ac.setVisible(True)		
 		wind = PFSWindow()
 		self.setCentralWidget(wind)
 
